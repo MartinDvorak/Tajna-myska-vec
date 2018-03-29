27e7a47da33301e97274a6bde203919e9b0114eb
MartinDvorak <xdvorakmartin@gmail.com>
2018-03-30 00:58:42 +0200

    merge

--- IJA_project/src/BlockSchemes/BlockSchemes.java
+++ IJA_project/src/BlockSchemes/BlockSchemes.java
@@ -7,16 +7,8 @@ public class BlockSchemes {
 
     public static void main(String[] args)
     {
-<<<<<<< HEAD
         //System.out.println("Hell");
-=======
-       // System.out.println("Hell");
-<<<<<<< HEAD
         HW2_tests test = new HW2_tests();
-
-=======
->>>>>>> 524e335a6f0e1c800b5627ee8ea98fa35bee1a91
->>>>>>> 5bd86e6b467c08c219898e016c6674c0489f509a
     }
 
 }
