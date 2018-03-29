24018ac12fbf207fc37399f8d044df6df4a1994f
MartinDvorak <xdvorakmartin@gmail.com>
2018-03-29 19:57:09 +0200

    init

---
+++ IJA_project/src/blocks_schemes/Blocks_schemes.java
@@ -0,0 +1,10 @@
+package blocks_schemes;
+
+public class Blocks_schemes {
+
+    public static void main(String[] args)
+    {
+        System.out.println("hell");
+    }
+
+}
---
+++ IJA_project/src/tests/HW2_tests.java
@@ -0,0 +1,45 @@
+package tests;
+
+import java.lang.reflect.Modifier;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class HW2_tests {
+
+    @Before
+    public void setUp()
+    {
+        //TODO
+    }
+
+    /**
+     * TODO
+     */
+    @Test
+    public void test01()
+    {
+        //TODO
+    }
+
+    /**
+     * TODO
+     */
+    @Test
+    public void test02()
+    {
+        //TODO
+    }
+
+    /**
+     * TODO
+     */
+    @Test
+    public void test03()
+    {
+        //TODO
+    }
+
+
+
+}
