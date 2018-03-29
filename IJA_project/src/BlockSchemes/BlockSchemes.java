1f6eb9dd80247a4e2540a8b01db6fb22e01dec0a
A-z-z-i <halvavl@seznam.cz>
2018-03-30 00:42:51 +0200

    haha

--- IJA_project/src/BlockSchemes/BlockSchemes.java
+++ IJA_project/src/BlockSchemes/BlockSchemes.java
@@ -1,10 +1,11 @@
 package BlockSchemes;
 
+
 public class BlockSchemes {
 
     public static void main(String[] args)
     {
-        System.out.println("hell");
+        //System.out.println("Hell");
     }
 
 }
