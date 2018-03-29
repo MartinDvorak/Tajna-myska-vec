b5d15491f5dfe03b6a147729ac00c955f7ab48ef
MartinDvorak <xdvorakmartin@gmail.com>
2018-03-30 00:54:36 +0200

    .sh

--- IJA_project/src/BlockSchemes/BlockSchemes.java
+++ IJA_project/src/BlockSchemes/BlockSchemes.java
@@ -1,11 +1,15 @@
 package BlockSchemes;
 
 
+import tests.HW2_tests;
+
 public class BlockSchemes {
 
     public static void main(String[] args)
     {
        // System.out.println("Hell");
+        HW2_tests test = new HW2_tests();
+
     }
 
 }
--- IJA_project/src/tests/HW2_tests.java
+++ IJA_project/src/tests/HW2_tests.java
@@ -11,6 +11,7 @@ public class HW2_tests {
     public void setUp()
     {
         //TODO
+        System.out.println("HELLL YEAH");
     }
 
     /**
